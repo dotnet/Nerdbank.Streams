8068da3fe97862cd271036ce48fec25ec5d3f4ec
Andrew Arnott <andarno@microsoft.com>
2015-10-06 19:46:02 -0700

    Adds missing docs to fix compiler warnings.

--- src/Nerdbank.FullDuplexStream/FullDuplexStream.cs
+++ src/Nerdbank.FullDuplexStream/FullDuplexStream.cs
@@ -7,6 +7,10 @@
     using System.Text;
     using Validation;
 
+    /// <summary>
+    /// Provides a full duplex stream which may be shared by two parties to
+    /// exchange messages.
+    /// </summary>
     public class FullDuplexStream : Stream
     {
         /// <summary>
@@ -15,17 +19,22 @@
         /// </summary>
         private readonly List<Message> readQueue = new List<Message>();
 
+        /// <inheritdoc />
         public override bool CanRead => true;
 
+        /// <inheritdoc />
         public override bool CanSeek => false;
 
+        /// <inheritdoc />
         public override bool CanWrite => true;
 
+        /// <inheritdoc />
         public override long Length
         {
             get { throw new NotSupportedException(); }
         }
 
+        /// <inheritdoc />
         public override long Position
         {
             get { throw new NotSupportedException(); }
@@ -34,6 +43,11 @@
 
         private FullDuplexStream other;
 
+        /// <summary>
+        /// Creates a pair of streams that can be passed to two parties
+        /// to allow for interaction with each other.
+        /// </summary>
+        /// <returns>A pair of streams.</returns>
         public static Tuple<Stream, Stream> CreateStreams()
         {
             var stream1 = new FullDuplexStream();
@@ -43,10 +57,12 @@
             return Tuple.Create<Stream, Stream>(stream1, stream2);
         }
 
+        /// <inheritdoc />
         public override void Flush()
         {
         }
 
+        /// <inheritdoc />
         public override int Read(byte[] buffer, int offset, int count)
         {
             Requires.NotNull(buffer, nameof(buffer));
@@ -54,9 +70,20 @@
             Requires.Range(count >= 0, nameof(count));
             Requires.Range(offset + count <= buffer.Length, nameof(count));
 
-            return 0;
+            lock (this.readQueue)
+            {
+                var message = this.readQueue[0];
+                int copiedBytes = message.Consume(buffer, offset, count);
+                if (message.IsConsumed)
+                {
+                    this.readQueue.RemoveAt(0);
+                }
+
+                return copiedBytes;
+            }
         }
 
+        /// <inheritdoc />
         public override void Write(byte[] buffer, int offset, int count)
         {
             Requires.NotNull(buffer, nameof(buffer));
@@ -72,11 +99,13 @@
             }
         }
 
+        /// <inheritdoc />
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public override void SetLength(long value)
         {
             throw new NotSupportedException();
@@ -89,21 +118,26 @@
                 this.Buffer = buffer;
             }
 
+            public bool IsConsumed => this.Position == this.Buffer.Length;
+
             /// <summary>
             /// Gets the buffer to read from.
             /// </summary>
-            public byte[] Buffer { get; }
+            private byte[] Buffer { get; }
 
             /// <summary>
             /// Gets or sets the position within the buffer that indicates the first
             /// character that has not yet been read.
             /// </summary>
-            public int Position { get; private set; }
+            private int Position { get; set; }
 
-            public void Consume(int bytesRead)
+            public int Consume(byte[] buffer, int offset, int count)
             {
-                Requires.Range(bytesRead >= 0, nameof(bytesRead));
-                this.Position += bytesRead;
+                int copiedBytes = Math.Min(count, this.Buffer.Length - this.Position);
+                Array.Copy(this.Buffer, this.Position, buffer, offset, copiedBytes);
+                this.Position += copiedBytes;
+                Assumes.False(this.Position > this.Buffer.Length);
+                return copiedBytes;
             }
         }
     }
