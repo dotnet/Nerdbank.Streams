0b0363f377d46137031fc276e6f9f87998d0ce6a
Andrew Arnott <andrewarnott@gmail.com>
2018-08-10 08:13:51 -0700

    Distribute extension methods across more classes

---
+++ src/Nerdbank.Streams/PipeExtensions.cs
@@ -0,0 +1,265 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace Nerdbank.Streams
+{
+    using System;
+    using System.IO;
+    using System.IO.Pipelines;
+    using System.Net.WebSockets;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft;
+    using Microsoft.VisualStudio.Threading;
+
+    /// <summary>
+    /// Stream extension methods.
+    /// </summary>
+    public static class PipeExtensions
+    {
+        /// <summary>
+        /// Exposes a full-duplex pipe as a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="pipe">The pipe to wrap as a stream.</param>
+        /// <returns>The wrapping stream.</returns>
+        public static Stream AsStream(this IDuplexPipe pipe) => new PipeStream(pipe);
+
+        /// <summary>
+        /// Exposes a pipe reader as a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="pipeReader">The pipe to read from when <see cref="Stream.ReadAsync(byte[], int, int, CancellationToken)"/> is invoked.</param>
+        /// <returns>The wrapping stream.</returns>
+        public static Stream AsStream(this PipeReader pipeReader) => new PipeStream(pipeReader);
+
+        /// <summary>
+        /// Exposes a pipe writer as a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="pipeWriter">The pipe to write to when <see cref="Stream.WriteAsync(byte[], int, int, CancellationToken)"/> is invoked.</param>
+        /// <returns>The wrapping stream.</returns>
+        public static Stream AsStream(this PipeWriter pipeWriter) => new PipeStream(pipeWriter);
+
+        /// <summary>
+        /// Enables efficiently reading a stream using <see cref="PipeReader"/>.
+        /// </summary>
+        /// <param name="stream">The stream to read from using a pipe.</param>
+        /// <param name="sizeHint">The size of the buffer to ask the stream to fill.</param>
+        /// <param name="cancellationToken">A cancellation token that aborts reading from the <paramref name="stream"/>.</param>
+        /// <returns>A <see cref="PipeReader"/>.</returns>
+        public static PipeReader UsePipeReader(this Stream stream, int sizeHint = 0, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(stream, nameof(stream));
+            Requires.Argument(stream.CanRead, nameof(stream), "Stream must be readable.");
+
+            var pipe = new Pipe();
+            Task.Run(async delegate
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Memory<byte> memory = pipe.Writer.GetMemory(sizeHint);
+                    try
+                    {
+                        int bytesRead = await stream.ReadAsync(memory, cancellationToken);
+                        if (bytesRead == 0)
+                        {
+                            break;
+                        }
+
+                        pipe.Writer.Advance(bytesRead);
+                    }
+                    catch (Exception ex)
+                    {
+                        pipe.Writer.Complete(ex);
+                        throw;
+                    }
+
+                    FlushResult result = await pipe.Writer.FlushAsync();
+                    if (result.IsCompleted)
+                    {
+                        break;
+                    }
+                }
+
+                // Tell the PipeReader that there's no more data coming
+                pipe.Writer.Complete();
+            }).Forget();
+            return pipe.Reader;
+        }
+
+        /// <summary>
+        /// Enables writing to a stream using <see cref="PipeWriter"/>.
+        /// </summary>
+        /// <param name="stream">The stream to write to using a pipe.</param>
+        /// <param name="cancellationToken">A cancellation token that aborts writing to the <paramref name="stream"/>.</param>
+        /// <returns>A <see cref="PipeWriter"/>.</returns>
+        public static PipeWriter UsePipeWriter(this Stream stream, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(stream, nameof(stream));
+            Requires.Argument(stream.CanWrite, nameof(stream), "Stream must be writable.");
+
+            var pipe = new Pipe();
+            Task.Run(async delegate
+            {
+                try
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        ReadResult readResult = await pipe.Reader.ReadAsync(cancellationToken);
+                        if (readResult.Buffer.Length > 0)
+                        {
+                            foreach (ReadOnlyMemory<byte> segment in readResult.Buffer)
+                            {
+                                await stream.WriteAsync(segment, cancellationToken);
+                            }
+
+                            await stream.FlushAsync(cancellationToken).ConfigureAwait(false);
+                        }
+
+                        pipe.Reader.AdvanceTo(readResult.Buffer.End);
+
+                        if (readResult.IsCompleted)
+                        {
+                            break;
+                        }
+                    }
+
+                    pipe.Reader.Complete();
+                }
+                catch (Exception ex)
+                {
+                    pipe.Reader.Complete(ex);
+                    throw;
+                }
+            }).Forget();
+            return pipe.Writer;
+        }
+
+        /// <summary>
+        /// Enables reading and writing to a <see cref="Stream"/> using <see cref="PipeWriter"/> and <see cref="PipeReader"/>.
+        /// </summary>
+        /// <param name="stream">The stream to access using a pipe.</param>
+        /// <param name="sizeHint">A hint at the size of messages that may be transferred. Use 0 for a commonly reasonable default.</param>
+        /// <param name="cancellationToken">A token that may cancel async processes to read from and write to the <paramref name="stream"/>.</param>
+        /// <returns>An <see cref="IDuplexPipe"/> instance.</returns>
+        public static IDuplexPipe UsePipe(this Stream stream, int sizeHint = 0, CancellationToken cancellationToken = default)
+        {
+            return new DuplexPipe(stream.UsePipeReader(sizeHint, cancellationToken), stream.UsePipeWriter(cancellationToken));
+        }
+
+        /// <summary>
+        /// Enables efficiently reading a <see cref="WebSocket"/> using <see cref="PipeReader"/>.
+        /// </summary>
+        /// <param name="webSocket">The web socket to read from using a pipe.</param>
+        /// <param name="sizeHint">The size of the buffer to ask the stream to fill.</param>
+        /// <param name="cancellationToken">A cancellation token that aborts reading from the <paramref name="webSocket"/>.</param>
+        /// <returns>A <see cref="PipeReader"/>.</returns>
+        public static PipeReader UsePipeReader(this WebSocket webSocket, int sizeHint = 0, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(webSocket, nameof(webSocket));
+
+            var pipe = new Pipe();
+            Task.Run(async delegate
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Memory<byte> memory = pipe.Writer.GetMemory(sizeHint);
+                    try
+                    {
+                        var readResult = await webSocket.ReceiveAsync(memory, cancellationToken);
+                        if (readResult.Count == 0)
+                        {
+                            break;
+                        }
+
+                        pipe.Writer.Advance(readResult.Count);
+                    }
+                    catch (Exception ex)
+                    {
+                        pipe.Writer.Complete(ex);
+                        throw;
+                    }
+
+                    FlushResult result = await pipe.Writer.FlushAsync();
+                    if (result.IsCompleted)
+                    {
+                        break;
+                    }
+                }
+
+                // Tell the PipeReader that there's no more data coming
+                pipe.Writer.Complete();
+            }).Forget();
+
+            return pipe.Reader;
+        }
+
+        /// <summary>
+        /// Enables efficiently writing to a <see cref="WebSocket"/> using a <see cref="PipeWriter"/>.
+        /// </summary>
+        /// <param name="webSocket">The web socket to write to using a pipe.</param>
+        /// <param name="cancellationToken">A cancellation token that aborts writing to the <paramref name="webSocket"/>.</param>
+        /// <returns>A <see cref="PipeWriter"/>.</returns>
+        public static PipeWriter UsePipeWriter(this WebSocket webSocket, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(webSocket, nameof(webSocket));
+
+            var pipe = new Pipe();
+            Task.Run(async delegate
+            {
+                try
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        ReadResult readResult = await pipe.Reader.ReadAsync(cancellationToken);
+                        if (readResult.Buffer.Length > 0)
+                        {
+                            foreach (ReadOnlyMemory<byte> segment in readResult.Buffer)
+                            {
+                                await webSocket.SendAsync(segment, WebSocketMessageType.Binary, endOfMessage: true, cancellationToken);
+                            }
+                        }
+
+                        pipe.Reader.AdvanceTo(readResult.Buffer.End);
+
+                        if (readResult.IsCompleted)
+                        {
+                            break;
+                        }
+                    }
+
+                    pipe.Reader.Complete();
+                }
+                catch (Exception ex)
+                {
+                    pipe.Reader.Complete(ex);
+                    throw;
+                }
+            }).Forget();
+            return pipe.Writer;
+        }
+
+        /// <summary>
+        /// Enables reading and writing to a <see cref="WebSocket"/> using <see cref="PipeWriter"/> and <see cref="PipeReader"/>.
+        /// </summary>
+        /// <param name="webSocket">The <see cref="WebSocket"/> to access using a pipe.</param>
+        /// <param name="sizeHint">A hint at the size of messages that may be transferred. Use 0 for a commonly reasonable default.</param>
+        /// <param name="cancellationToken">A token that may cancel async processes to read from and write to the <paramref name="webSocket"/>.</param>
+        /// <returns>An <see cref="IDuplexPipe"/> instance.</returns>
+        public static IDuplexPipe UsePipe(this WebSocket webSocket, int sizeHint = 0, CancellationToken cancellationToken = default)
+        {
+            return new DuplexPipe(webSocket.UsePipeReader(sizeHint, cancellationToken), webSocket.UsePipeWriter(cancellationToken));
+        }
+
+        private class DuplexPipe : IDuplexPipe
+        {
+            internal DuplexPipe(PipeReader input, PipeWriter output)
+            {
+                this.Input = input ?? throw new ArgumentNullException(nameof(input));
+                this.Output = output ?? throw new ArgumentNullException(nameof(output));
+            }
+
+            public PipeReader Input { get; }
+
+            public PipeWriter Output { get; }
+        }
+    }
+}
---
+++ src/Nerdbank.Streams/SpanPolyfillExtensions.cs
@@ -0,0 +1,181 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+#pragma warning disable AvoidAsyncSuffix // Avoid Async suffix
+#if !SPAN_BUILTIN
+
+namespace Nerdbank.Streams
+{
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Pipelines;
+    using System.Net.WebSockets;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft;
+    using Microsoft.VisualStudio.Threading;
+
+    internal static class SpanPolyfillExtensions
+    {
+        /// <summary>
+        /// Reads from the stream into a memory buffer.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <param name="buffer">The buffer to read directly into.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>The number of bytes actually read.</returns>
+        /// <devremarks>
+        /// This method shamelessly copied from the .NET Core 2.1 Stream class: https://github.com/dotnet/coreclr/blob/a113b1c803783c9d64f1f0e946ff9a853e3bc140/src/System.Private.CoreLib/shared/System/IO/Stream.cs#L366-L391.
+        /// </devremarks>
+        internal static ValueTask<int> ReadAsync(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(stream, nameof(stream));
+
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return new ValueTask<int>(stream.ReadAsync(array.Array, array.Offset, array.Count, cancellationToken));
+            }
+            else
+            {
+                byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                return FinishReadAsync(stream.ReadAsync(sharedBuffer, 0, buffer.Length, cancellationToken), sharedBuffer, buffer);
+
+                async ValueTask<int> FinishReadAsync(Task<int> readTask, byte[] localBuffer, Memory<byte> localDestination)
+                {
+                    try
+                    {
+                        int result = await readTask.ConfigureAwait(false);
+                        new Span<byte>(localBuffer, 0, result).CopyTo(localDestination.Span);
+                        return result;
+                    }
+                    finally
+                    {
+                        ArrayPool<byte>.Shared.Return(localBuffer);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes to a stream from a memory buffer.
+        /// </summary>
+        /// <param name="stream">The stream to write to.</param>
+        /// <param name="buffer">The buffer to read from.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A task that indicates when the write operation is complete.</returns>
+        /// <devremarks>
+        /// This method shamelessly copied from the .NET Core 2.1 Stream class: https://github.com/dotnet/coreclr/blob/a113b1c803783c9d64f1f0e946ff9a853e3bc140/src/System.Private.CoreLib/shared/System/IO/Stream.cs#L672-L696.
+        /// </devremarks>
+        internal static ValueTask WriteAsync(this Stream stream, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(stream, nameof(stream));
+
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return new ValueTask(stream.WriteAsync(array.Array, array.Offset, array.Count, cancellationToken));
+            }
+            else
+            {
+                byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                buffer.Span.CopyTo(sharedBuffer);
+                return new ValueTask(FinishWriteAsync(stream.WriteAsync(sharedBuffer, 0, buffer.Length, cancellationToken), sharedBuffer));
+            }
+
+            async Task FinishWriteAsync(Task writeTask, byte[] localBuffer)
+            {
+                try
+                {
+                    await writeTask.ConfigureAwait(false);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(localBuffer);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads from the stream into a memory buffer.
+        /// </summary>
+        /// <param name="webSocket">The stream to read from.</param>
+        /// <param name="buffer">The buffer to read directly into.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>The number of bytes actually read.</returns>
+        /// <devremarks>
+        /// This method shamelessly copied from the .NET Core 2.1 Stream class: https://github.com/dotnet/coreclr/blob/a113b1c803783c9d64f1f0e946ff9a853e3bc140/src/System.Private.CoreLib/shared/System/IO/Stream.cs#L366-L391.
+        /// </devremarks>
+        internal static ValueTask<WebSocketReceiveResult> ReceiveAsync(this WebSocket webSocket, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(webSocket, nameof(webSocket));
+
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return new ValueTask<WebSocketReceiveResult>(webSocket.ReceiveAsync(array, cancellationToken));
+            }
+            else
+            {
+                byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                return FinishReadAsync(webSocket.ReceiveAsync(new ArraySegment<byte>(sharedBuffer, 0, buffer.Length), cancellationToken), sharedBuffer, buffer);
+
+                async ValueTask<WebSocketReceiveResult> FinishReadAsync(Task<WebSocketReceiveResult> readTask, byte[] localBuffer, Memory<byte> localDestination)
+                {
+                    try
+                    {
+                        WebSocketReceiveResult result = await readTask.ConfigureAwait(false);
+                        new Span<byte>(localBuffer, 0, result.Count).CopyTo(localDestination.Span);
+                        return result;
+                    }
+                    finally
+                    {
+                        ArrayPool<byte>.Shared.Return(localBuffer);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes to a stream from a memory buffer.
+        /// </summary>
+        /// <param name="webSocket">The stream to write to.</param>
+        /// <param name="buffer">The buffer to read from.</param>
+        /// <param name="messageType">The type of WebSocket message.</param>
+        /// <param name="endOfMessage">Whether to signify that this write operation concludes a "message" in the semantic of whatever protocol is being used.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A task that indicates when the write operation is complete.</returns>
+        /// <devremarks>
+        /// This method shamelessly copied from the .NET Core 2.1 Stream class: https://github.com/dotnet/coreclr/blob/a113b1c803783c9d64f1f0e946ff9a853e3bc140/src/System.Private.CoreLib/shared/System/IO/Stream.cs#L672-L696.
+        /// </devremarks>
+        internal static ValueTask SendAsync(this WebSocket webSocket, ReadOnlyMemory<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken = default)
+        {
+            Requires.NotNull(webSocket, nameof(webSocket));
+
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return new ValueTask(webSocket.SendAsync(array, messageType, endOfMessage, cancellationToken));
+            }
+            else
+            {
+                byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                buffer.Span.CopyTo(sharedBuffer);
+                return new ValueTask(FinishWriteAsync(webSocket.SendAsync(new ArraySegment<byte>(sharedBuffer, 0, buffer.Length), messageType, endOfMessage, cancellationToken), sharedBuffer));
+            }
+
+            async Task FinishWriteAsync(Task writeTask, byte[] localBuffer)
+            {
+                try
+                {
+                    await writeTask.ConfigureAwait(false);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(localBuffer);
+                }
+            }
+        }
+    }
+}
+
+#endif
