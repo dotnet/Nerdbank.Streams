6379c47a34db743289fab57388ca33af5099150d
Andrew Arnott <andrewarnott@gmail.com>
2019-03-17 21:47:06 -0700

    Benchmark measures memory allocations

--- src/Nerdbank.Streams.Benchmark/SequenceBenchmark.cs
+++ src/Nerdbank.Streams.Benchmark/SequenceBenchmark.cs
@@ -9,62 +9,57 @@ namespace Nerdbank.Streams.Benchmark
     using System.Text;
     using BenchmarkDotNet.Attributes;
 
+    [Config(typeof(BenchmarkConfig))]
     public class SequenceBenchmark
     {
+        private readonly Sequence<byte> sequence = new Sequence<byte>();
+
         [Benchmark]
         public void OneSegment_GetMemory()
         {
-            using (var sequence = new Sequence<byte>())
-            {
-                var mem = sequence.GetMemory(12);
-                sequence.Advance(4);
-                mem = sequence.GetMemory(8);
-                sequence.Advance(4);
-                var ros = sequence.AsReadOnlySequence;
-            }
+            var mem = this.sequence.GetMemory(12);
+            this.sequence.Advance(4);
+            mem = this.sequence.GetMemory(8);
+            this.sequence.Advance(4);
+            var ros = this.sequence.AsReadOnlySequence;
+            this.sequence.Reset();
         }
 
         [Benchmark]
         public void OneSegment_GetSpan()
         {
-            using (var sequence = new Sequence<byte>())
-            {
-                var span = sequence.GetSpan(12);
-                sequence.Advance(4);
-                span = sequence.GetSpan(8);
-                sequence.Advance(4);
-                var ros = sequence.AsReadOnlySequence;
-            }
+            var span = this.sequence.GetSpan(12);
+            this.sequence.Advance(4);
+            span = this.sequence.GetSpan(8);
+            this.sequence.Advance(4);
+            var ros = this.sequence.AsReadOnlySequence;
+            this.sequence.Reset();
         }
 
         [Benchmark]
         public void MultiSegment_GetMemory()
         {
-            using (var sequence = new Sequence<byte>())
-            {
-                var mem = sequence.GetMemory(12);
-                sequence.Advance(10);
-                mem = sequence.GetMemory(128);
-                sequence.Advance(120);
-                mem = sequence.GetMemory(256);
-                sequence.Advance(250);
-                var ros = sequence.AsReadOnlySequence;
-            }
+            var mem = this.sequence.GetMemory(12);
+            this.sequence.Advance(10);
+            mem = this.sequence.GetMemory(128);
+            this.sequence.Advance(120);
+            mem = this.sequence.GetMemory(256);
+            this.sequence.Advance(250);
+            var ros = this.sequence.AsReadOnlySequence;
+            this.sequence.Reset();
         }
 
         [Benchmark]
         public void MultiSegment_GetSpan()
         {
-            using (var sequence = new Sequence<byte>())
-            {
-                var span = sequence.GetSpan(12);
-                sequence.Advance(10);
-                span = sequence.GetSpan(128);
-                sequence.Advance(120);
-                span = sequence.GetSpan(256);
-                sequence.Advance(250);
-                var ros = sequence.AsReadOnlySequence;
-            }
+            var span = this.sequence.GetSpan(12);
+            this.sequence.Advance(10);
+            span = this.sequence.GetSpan(128);
+            this.sequence.Advance(120);
+            span = this.sequence.GetSpan(256);
+            this.sequence.Advance(250);
+            var ros = this.sequence.AsReadOnlySequence;
+            this.sequence.Reset();
         }
     }
 }
