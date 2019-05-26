d2692984c45a75bea9e1ef6eab4b63cb6f8feecf
asmichi <asmichi@users.noreply.github.com>
2019-05-26 15:10:10 +0900

    Add test for propagating exceptions from underlying streams.

---
+++ src/Nerdbank.Streams.Tests/MockInterruptedFullDuplexStream.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+using System;
+using System.IO;
+
+// Represents a full-duplex Stream that is in an erroneous state and throws IOException.
+internal class MockInterruptedFullDuplexStream : Stream
+{
+    public override bool CanRead => true;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => throw new NotSupportedException();
+
+    public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+    public override void Flush() => throw new IOException();
+
+    public override int Read(byte[] buffer, int offset, int count) => throw new IOException();
+
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+    public override void SetLength(long value) => throw new NotSupportedException();
+
+    public override void Write(byte[] buffer, int offset, int count) => throw new IOException();
+}
