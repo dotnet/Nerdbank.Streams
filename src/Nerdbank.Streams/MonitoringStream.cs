ee389e1c44eafcbc32e5ac1e54908194c3a2b427
Andrew Arnott <andrewarnott@gmail.com>
2019-09-21 12:29:53 -0600

    Capture use of new .NET Core 2.1 methods on MonitoringStream

--- src/Nerdbank.Streams/MonitoringStream.cs
+++ src/Nerdbank.Streams/MonitoringStream.cs
@@ -5,6 +5,7 @@ namespace Nerdbank.Streams
 {
     using System;
     using System.IO;
+    using System.Runtime.InteropServices;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -28,6 +29,20 @@ namespace Nerdbank.Streams
         }
 
         /// <summary>
+        /// A delegate used for events that pass <see cref="Span{T}"/> around.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="span">The span.</param>
+        public delegate void SpanEventHandler(object sender, Span<byte> span);
+
+        /// <summary>
+        /// A delegate used for events that pass <see cref="ReadOnlySpan{T}"/> around.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="span">The span.</param>
+        public delegate void ReadOnlySpanEventHandler(object sender, ReadOnlySpan<byte> span);
+
+        /// <summary>
         /// Occurs after <see cref="Seek(long, SeekOrigin)"/> is invoked.
         /// </summary>
         public event EventHandler<long>? DidSeek;
@@ -47,6 +62,55 @@ namespace Nerdbank.Streams
         /// The <see cref="ArraySegment{T}.Count"/> value is the actual bytes that were read.
         /// </remarks>
         public event EventHandler<ArraySegment<byte>>? DidRead;
+
+#pragma warning disable CS1574
+#pragma warning disable CS0067 // Only .NET Core 2.1 raises these
+
+        /// <summary>
+        /// Occurs before <see cref="ReadAsync(Memory{byte}, CancellationToken)"/> is invoked.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="Memory{T}.Length"/> value is the maximum bytes that may be read.
+        /// </remarks>
+        public event EventHandler<Memory<byte>>? WillReadMemory;
+
+        /// <summary>
+        /// Occurs after <see cref="ReadAsync(Memory{byte}, CancellationToken)"/> is invoked.
+        /// </summary>
+        public event EventHandler<Memory<byte>>? DidReadMemory;
+
+        /// <summary>
+        /// Occurs before <see cref="Read(Span{byte})"/> is invoked.
+        /// </summary>
+        public event SpanEventHandler? WillReadSpan;
+
+        /// <summary>
+        /// Occurs after <see cref="Read(Span{byte})"/> is invoked.
+        /// </summary>
+        public event SpanEventHandler? DidReadSpan;
+
+        /// <summary>
+        /// Occurs before <see cref="Write(ReadOnlySpan{byte})"/> is invoked.
+        /// </summary>
+        public event ReadOnlySpanEventHandler? WillWriteSpan;
+
+        /// <summary>
+        /// Occurs after <see cref="Write(ReadOnlySpan{byte})"/> is invoked.
+        /// </summary>
+        public event ReadOnlySpanEventHandler? DidWriteSpan;
+
+        /// <summary>
+        /// Occurs before <see cref="WriteAsync(ReadOnlyMemory{byte}, CancellationToken)"/> is invoked.
+        /// </summary>
+        public event EventHandler<ReadOnlyMemory<byte>>? WillWriteMemory;
+
+        /// <summary>
+        /// Occurs after <see cref="WriteAsync(ReadOnlyMemory{byte}, CancellationToken)"/> is invoked.
+        /// </summary>
+        public event EventHandler<ReadOnlyMemory<byte>>? DidWriteMemory;
+
+#pragma warning restore CS0067 // Only .NET Core 2.1 raises these
+#pragma warning restore CS1574
 
         /// <summary>
         /// Occurs before <see cref="Write(byte[], int, int)"/> or <see cref="WriteAsync(byte[], int, int, CancellationToken)"/> is invoked.
@@ -152,6 +216,44 @@ namespace Nerdbank.Streams
             this.DidRead?.Invoke(this, new ArraySegment<byte>(buffer, offset, bytesRead));
             return bytesRead;
         }
+
+#if SPAN_BUILTIN
+
+        /// <inheritdoc/>
+        public override int Read(Span<byte> buffer)
+        {
+            this.WillReadSpan?.Invoke(this, buffer);
+            int bytesRead = base.Read(buffer);
+            this.DidReadSpan?.Invoke(this, buffer);
+            return bytesRead;
+        }
+
+        /// <inheritdoc/>
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            this.WillReadMemory?.Invoke(this, buffer);
+            int bytesRead = await base.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
+            this.DidReadMemory?.Invoke(this, buffer);
+            return bytesRead;
+        }
+
+        /// <inheritdoc/>
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            this.WillWriteSpan?.Invoke(this, buffer);
+            base.Write(buffer);
+            this.DidWriteSpan?.Invoke(this, buffer);
+        }
+
+        /// <inheritdoc/>
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            this.WillWriteMemory?.Invoke(this, buffer);
+            await base.WriteAsync(buffer, cancellationToken);
+            this.DidWriteMemory?.Invoke(this, buffer);
+        }
+
+#endif
 
         /// <inheritdoc/>
         public override long Seek(long offset, SeekOrigin origin)
