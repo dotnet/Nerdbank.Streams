ba05aa84c04a89bd0c35f6d27a922e89e52a9ae8
Andrew Arnott <andrewarnott@gmail.com>
2020-06-25 20:42:24 -0600

    Add synchronous MultiplexingStream.Create method for v3

--- src/Nerdbank.Streams.Tests/MultiplexingStreamV3Tests.cs
+++ src/Nerdbank.Streams.Tests/MultiplexingStreamV3Tests.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Threading.Tasks;
+using Nerdbank.Streams;
+using Xunit;
 using Xunit.Abstractions;
 
 public class MultiplexingStreamV3Tests : MultiplexingStreamV2Tests
@@ -11,4 +15,28 @@ public class MultiplexingStreamV3Tests : MultiplexingStreamV2Tests
     }
 
     protected override int ProtocolMajorVersion => 3;
+
+    /// <summary>
+    /// Verify the <see cref="MultiplexingStream.Create(System.IO.Stream, MultiplexingStream.Options?)"/> method,
+    /// since all the inherited tests run based on the <see cref="MultiplexingStream.CreateAsync(System.IO.Stream, MultiplexingStream.Options?, System.Threading.CancellationToken)"/> method.
+    /// </summary>
+    [Fact]
+    public async Task Create()
+    {
+        var pair = FullDuplexStream.CreatePair();
+        var mx1 = MultiplexingStream.Create(pair.Item1);
+        var mx2 = MultiplexingStream.Create(pair.Item2);
+
+        await Task.WhenAll(mx1.OfferChannelAsync("test"), mx2.AcceptChannelAsync("test"));
+        await mx1.DisposeAsync();
+        await mx2.DisposeAsync();
+    }
+
+    [Fact]
+    public void Create_VersionsWithHandshakes()
+    {
+        var pair = FullDuplexStream.CreatePair();
+        Assert.Throws<NotSupportedException>(() => MultiplexingStream.Create(pair.Item1, new MultiplexingStream.Options { ProtocolMajorVersion = 1 }));
+        Assert.Throws<NotSupportedException>(() => MultiplexingStream.Create(pair.Item1, new MultiplexingStream.Options { ProtocolMajorVersion = 2 }));
+    }
 }
