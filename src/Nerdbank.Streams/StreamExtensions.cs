2b142fa6a38b15e4b06ecc53bf073aa49fd1de34
Andrew Arnott <andrewarnott@live.com>
2021-03-30 13:02:28 -0600

    Add `ROS<byte>.AsStream` overload that takes a disposing callback

--- src/Nerdbank.Streams/StreamExtensions.cs
+++ src/Nerdbank.Streams/StreamExtensions.cs
@@ -36,7 +36,16 @@ namespace Nerdbank.Streams
         /// </summary>
         /// <param name="readOnlySequence">The sequence of bytes to expose as a stream.</param>
         /// <returns>The readable stream.</returns>
-        public static Stream AsStream(this ReadOnlySequence<byte> readOnlySequence) => new ReadOnlySequenceStream(readOnlySequence);
+        public static Stream AsStream(this ReadOnlySequence<byte> readOnlySequence) => new ReadOnlySequenceStream(readOnlySequence, null, null);
+
+        /// <summary>
+        /// Exposes a <see cref="ReadOnlySequence{T}"/> of <see cref="byte"/> as a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="readOnlySequence">The sequence of bytes to expose as a stream.</param>
+        /// <param name="disposeAction">A delegate to invoke when the returned stream is disposed.</param>
+        /// <param name="disposeActionArg">The argument to pass to <paramref name="disposeAction"/>.</param>
+        /// <returns>The readable stream.</returns>
+        public static Stream AsStream(this ReadOnlySequence<byte> readOnlySequence, Action<object?> disposeAction, object? disposeActionArg) => new ReadOnlySequenceStream(readOnlySequence, disposeAction, disposeActionArg);
 
         /// <summary>
         /// Creates a writable <see cref="Stream"/> that can be used to add to a <see cref="IBufferWriter{T}"/> of <see cref="byte"/>.
