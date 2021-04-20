24aa9d802ed585ed68806eb888a50f4920977ff8
Andrew Arnott <andrewarnott@live.com>
2021-04-20 15:57:17 -0600

    Honor writer cancellation/completion in LinkToAsync
    Fixes #251

--- src/Nerdbank.Streams/Strings.Designer.cs
+++ src/Nerdbank.Streams/Strings.Designer.cs
@@ -106,6 +106,24 @@ namespace Nerdbank.Streams {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The PipeReader was canceled..
+        /// </summary>
+        internal static string PipeReaderCanceled {
+            get {
+                return ResourceManager.GetString("PipeReaderCanceled", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The PipeWriter flush was canceled..
+        /// </summary>
+        internal static string PipeWriterFlushCanceled {
+            get {
+                return ResourceManager.GetString("PipeWriterFlushCanceled", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to No reading operation to complete..
         /// </summary>
         internal static string ReadBeforeAdvanceTo {
