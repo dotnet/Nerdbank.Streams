254a59a78e51b0fe7fb225bd51e966583ab6f2d0
Andrew Arnott <andrewarnott@live.com>
2021-11-18 08:59:36 -0700

    Rewrite `writeAsync` in terms of `Promise<void>`

--- src/nerdbank-streams/src/Utilities.ts
+++ src/nerdbank-streams/src/Utilities.ts
@@ -4,15 +4,15 @@ import { Deferred } from "./Deferred";
 import { IDisposableObservable } from "./IDisposableObservable";
 
 export async function writeAsync(stream: NodeJS.WritableStream, chunk: any) {
-    const deferred = new Deferred<void>();
-    stream.write(chunk, (err: Error | null | undefined) => {
-        if (err) {
-            deferred.reject(err);
-        } else {
-            deferred.resolve();
-        }
+    return new Promise<void>((resolve, reject) => {
+        stream.write(chunk, (err: Error | null | undefined) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
     });
-    return deferred.promise;
 }
 
 export function writeSubstream(stream: NodeJS.WritableStream): NodeJS.WritableStream {
@@ -25,7 +25,7 @@ export function writeSubstream(stream: NodeJS.WritableStream): NodeJS.WritableSt
                 await writeAsync(stream, chunk);
                 callback();
             } catch (err) {
-                callback(err);
+                callback(err as Error);
             }
         },
         final(callback: (error?: Error | null) => void) {
