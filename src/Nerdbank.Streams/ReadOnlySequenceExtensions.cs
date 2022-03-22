ec208997090d880bd72e473db680f7577d744dd3
Andrew Arnott <andrewarnott@live.com>
2022-03-22 16:56:54 -0600

    Add `ReadOnlySequence<T>.Clone()` extension method

--- src/Nerdbank.Streams/ReadOnlySequenceExtensions.cs
+++ src/Nerdbank.Streams/ReadOnlySequenceExtensions.cs
@@ -5,8 +5,28 @@ namespace Nerdbank.Streams;
 
 using System.Buffers;
 
-internal static class ReadOnlySequenceExtensions
+/// <summary>
+/// Extension methods for the <see cref="ReadOnlySequence{T}"/> type.
+/// </summary>
+public static class ReadOnlySequenceExtensions
 {
+    /// <summary>
+    /// Copies the content of one <see cref="ReadOnlySequence{T}"/> to another that is backed by its own
+    /// memory buffers.
+    /// </summary>
+    /// <typeparam name="T">The type of element in the sequence.</typeparam>
+    /// <param name="template">The sequence to copy from.</param>
+    /// <returns>A shallow copy of the sequence, backed by buffers which will never be recycled.</returns>
+    /// <remarks>
+    /// This method is useful for retaining data that is backed by buffers that will be reused later.
+    /// </remarks>
+    public static ReadOnlySequence<T> Clone<T>(this ReadOnlySequence<T> template)
+    {
+        Sequence<T> sequence = new();
+        sequence.Write(template);
+        return sequence;
+    }
+
     /// <summary>
     /// Polyfill method used by the <see cref="SequenceReader{T}"/>.
     /// </summary>
---
+++ test/Nerdbank.Streams.Tests/ReadOnlySequenceExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Buffers;
+using System.Runtime.InteropServices;
+using Nerdbank.Streams;
+using Xunit;
+
+public class ReadOnlySequenceExtensionsTests
+{
+    [Fact]
+    public void Clone()
+    {
+        int[] array = new[] { 1, 2, 3 };
+        Sequence<int> seq = new();
+        seq.Append(array);
+
+        ReadOnlySequence<int> copy = seq.AsReadOnlySequence.Clone();
+        Assert.Equal(array, copy.ToArray());
+        Assert.True(MemoryMarshal.TryGetArray(seq.AsReadOnlySequence.First, out ArraySegment<int> seqFirstSegment));
+        Assert.True(MemoryMarshal.TryGetArray(copy.First, out ArraySegment<int> copyFirstSegment));
+        Assert.NotSame(seqFirstSegment.Array, copyFirstSegment.Array);
+    }
+}
