b7f823f9dbfc9cc1631defb4f9dc5598e337260e
Andrew Arnott <andrewarnott@live.com>
2022-03-22 16:42:28 -0600

    Define `IBufferWriter<T>.Write(ReadOnlySequence<T>)` extension method

---
+++ src/Nerdbank.Streams/BufferWriterExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Nerdbank.Streams;
+
+using System.Buffers;
+using Microsoft;
+
+/// <summary>
+/// Extension methods for the <see cref="IBufferWriter{T}"/> interface.
+/// </summary>
+public static class BufferWriterExtensions
+{
+    /// <summary>
+    /// Copies the content of a <see cref="ReadOnlySequence{T}"/> into an <see cref="IBufferWriter{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of element to copy.</typeparam>
+    /// <param name="writer">The <see cref="IBufferWriter{T}"/> to write to.</param>
+    /// <param name="sequence">The sequence to read from.</param>
+    public static void Write<T>(this IBufferWriter<T> writer, ReadOnlySequence<T> sequence)
+    {
+        Requires.NotNull(writer, nameof(writer));
+
+        foreach (ReadOnlyMemory<T> memory in sequence)
+        {
+            writer.Write(memory.Span);
+        }
+    }
+}
---
+++ test/Nerdbank.Streams.Tests/BufferWriterExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Nerdbank.Streams.Tests;
+
+using System.Buffers;
+using Xunit;
+
+public class BufferWriterExtensionsTests
+{
+    [Fact]
+    public void Write_Default()
+    {
+        using Sequence<int> seq = new();
+        seq.Write(default);
+        Assert.Equal(0, seq.Length);
+    }
+
+    [Fact]
+    public void Write_OneBlock()
+    {
+        int[] array = new int[] { 1, 2, 3 };
+        using Sequence<int> template = new();
+        template.Write(array);
+
+        using Sequence<int> seq = new();
+        seq.Write(template);
+        Assert.Equal(array, seq.AsReadOnlySequence.ToArray());
+    }
+
+    [Fact]
+    public void Write_MultipleBlocks()
+    {
+        int[] array = new int[] { 1, 2, 3 };
+        using Sequence<int> template = new();
+        template.Write(array);
+        template.Append(array);
+
+        using Sequence<int> seq = new();
+        seq.Write(template);
+        Assert.Equal(array.Concat(array), seq.AsReadOnlySequence.ToArray());
+    }
+}
