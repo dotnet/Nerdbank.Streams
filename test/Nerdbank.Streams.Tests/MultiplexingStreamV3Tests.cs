782ad6d8ed214a430747ceb2b6027fabecc5c417
Devesh Sarda <32046390+sarda-devesh@users.noreply.github.com>
2022-08-25 00:20:50 -0700

    Revert namespace change

--- test/Nerdbank.Streams.Tests/MultiplexingStreamV3Tests.cs
+++ test/Nerdbank.Streams.Tests/MultiplexingStreamV3Tests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Nerdbank.Streams.Tests;
-
 using System;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Threading;
