92729e21ff57c68ffaf3793d067d8da8d106fbed
Juan Ruggero <64375140+JRuggero@users.noreply.github.com>
2025-03-02 18:30:20 -0300

    Update MonitoringStream.cs

--- src/Nerdbank.Streams/MonitoringStream.cs
+++ src/Nerdbank.Streams/MonitoringStream.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace MyMonitoringStream
+namespace Nerdbank.Streams
 {
     using System;
     using System.IO;
